f1c911d365f89b005bcbfc32e5b110a4e1d4c6d5
Giperbola <egor.crut2017@yandex.ru>
2025-09-19 22:19:58 +0300

    Cmake

--- Logisim/Main.cpp
+++ Logisim/Main.cpp
@@ -2,8 +2,16 @@
 #include <windows.h>
 
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+//    LPSTR lpCmdLine, int nCmdShow)
+//{
+//    Logisim app;
+//    app.run();
+//
+//    return 0;
+//}
+
+int main()
 {
     Logisim app;
     app.run();
